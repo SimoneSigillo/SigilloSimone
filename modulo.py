2c1865e4a3878f7f4039c3b8ee47185b32496cda
SimoneSigillo <74323374+SimoneSigillo@users.noreply.github.com>
2020-12-17 01:31:57 +0100

    Add files via upload

---
+++ modulo.py
@@ -0,0 +1,17 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: C:/Users/maris/Desktop/grafico py.py ================
+Benvenuto!
+
+-Premi 1 per installare una libreia
+-Premi 2 per disinstallare una libreria
+--> 1
+Come si chiama la tua libreria?  --> matplotlib
+
+
+
+-Premi invio per riutilizzare il programma o premi ctrl + c per terminarne  l'esecuzione  
+=============== RESTART: C:/Users/maris/Desktop/grafico python.py ==============
+>>> 
+=============== RESTART: C:/Users/maris/Desktop/grafico python.py ==============
