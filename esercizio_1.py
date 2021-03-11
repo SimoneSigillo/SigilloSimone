1108ef325ca05a97e4cdad4a0beeb52c03b2131d
SimoneSigillo <74323374+SimoneSigillo@users.noreply.github.com>
2021-03-11 01:34:59 +0100

    Add files via upload

---
+++ esercizio_1.py
@@ -0,0 +1,13 @@
+import numpy
+a= (input("Dimmi un numero"))
+b= (input("Dimmi un numero"))
+c= (input("Dimmi un numero"))
+d= (input("Dimmi un numero"))
+e= (input("Dimmi un numero"))
+lista=[a,b,c,d,e]
+x= numpy.array(lista)
+print(lista)
+lista.sort()
+print(lista)
+lista.reverse()
+print(lista)
---
+++ esercizio_3.py
@@ -0,0 +1,6 @@
+import numpy 
+lista_bidimensionale=numpy.array([[1,2,3],[4,5,6],[7,8,9]])
+print(lista_bidimensionale)
+x= int(input("Scegli quale visualizzare")) - 1 
+print(lista_bidimensionale[x])
+
