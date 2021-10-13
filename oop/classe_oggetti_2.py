8ac2c2b6bf5909554088c6b0b3988dc9a74a7418
SimoneSigillo <74323374+SimoneSigillo@users.noreply.github.com>
2021-10-14 00:32:08 +0200

    Add files via upload

---
+++ oop/classe_oggetti_2.py
@@ -0,0 +1,124 @@
+class calcComb():
+
+    def __init__(self, stringa):
+
+        self.__N = len(stringa)
+        self.__stringa = stringa
+        self.__listStringa = list(stringa)
+
+    def get_stringa(self):
+        return self.__stringa
+
+    def get_listStringa(self):
+        return self.__listStringa
+
+    def setStringa(self):
+        '''
+        modificare questo metodo in modo da verificare la coerenza delle variabili di
+        istanza presenti
+        '''
+                                                                                                        #??
+        return 0           
+
+    def confUtil(self):
+        '''
+        verificare se la STRINGA attributo di istanza è presente
+        nel file word.italian.txt
+        '''
+
+        x=open("word.italian.txt","r")                                                      #utilizzare la funzione open per collegare l'altro file e fare una condizione di if per vedere se la stringa abbia un significato o meno rispettando il file word.italian.txt
+        if termine in x:
+            print("La stringa ", termine, " ha significato")
+        else:
+            print("La stringa ", termine, " non ha significato")                   
+        
+            
+
+                                                                                        
+
+        return 0
+
+
+    # PERMUTAZIONI
+
+    def nPermutSenzaRip(self):
+        '''
+        restituire il numero di permutazioni SENZA ripetizione
+        '''
+        return 0
+
+    def nPermutConRip(self):
+        '''
+        restituire il numero di permutazioni CON ripetizione
+        '''
+        return 0
+
+    def permutSenzaRip(self):
+        '''
+        generare e restituire la lista di permutazioni SENZA ripetizione
+        '''
+        return 0
+
+    def permutConRip(self):
+        '''
+        generare e restituire la lista di permutazioni CON ripetizione
+        '''
+        return 0
+
+    # DISPOSIZIONI
+
+    def nDispSemplSenzaRip(self):
+        '''
+        restituire il numero di disposizioni semplici SENZA ripetizione
+        '''
+        return 0
+
+    def nDispSemplConRip(self):
+        '''
+        restituire il numero di disposizioni semplici CON ripetizione
+        '''
+        return 0
+
+    def dispSemplSenzaRip(self):
+        '''
+        generare e restituire la lista delle disposizioni semplici SENZA ripetizione
+        '''
+        return 0
+
+
+    def dispSemplConRip(self):
+        '''
+        generare e restituire la lista delle disposizioni semplici CON ripetizione
+        '''
+        return 0
+
+    # COMBINAZIONI
+
+    def nCombSemplSenzaRip(self):
+        '''
+        restituire il numero delle combinazioni SENZA ripetizione
+        '''
+        return 0
+
+    def nCombSemplConRip(self):
+        '''
+        restituire il numero delle combinazioni CON ripetizione
+        '''
+        return 0
+
+    def combSenzaRip(self):
+        '''
+        generare e restituire la lista delle combinazioni SENZA ripetizione
+        '''
+        return 0
+
+
+    def combConRip(self):
+        '''
+        generare e restituire la lista delle combinazioni CON ripetizione
+        '''
+        return 0
+
+termine=calcComb(str(input("Inserisci una parola: ")))
+termine.confUtil()
+
